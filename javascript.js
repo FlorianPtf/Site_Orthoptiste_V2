8c0993ee2dce26ed50202971e131c0ee0635f709
Florian Bruloy <bruloy.floriangmail.com>
2022-06-19 19:20:18 +0200

    ajout img bg, animation on scroll

--- javascript.js
+++ javascript.js
@@ -41,19 +41,20 @@ slider.addEventListener("mouseout", () => {
 
 
 
-const inViewport = (entries, observer) => {
-    entries.forEach(entry => {
-      entry.target.classList.toggle("is-inViewport", entry.isIntersecting);
-    });
-  };
+// const inViewport = (entries, observer) => {
+//     entries.forEach(entry => {
+//       entry.target.classList.toggle("is-inViewport", entry.isIntersecting);
+//     });
+//   };
   
-  const Obs = new IntersectionObserver(inViewport);
-  const obsOptions = {}; //See: https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API#Intersection_observer_options
+//   const Obs = new IntersectionObserver(inViewport);
+//   const obsOptions = {}; //See: https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API#Intersection_observer_options
   
-  // Attach observer to every [data-inviewport] element:
-  const ELs_inViewport = document.querySelectorAll('[body]');
-  ELs_inViewport.forEach(EL => {
-    Obs.observe(EL, obsOptions);
-  });
+//   // Attach observer to every [data-inviewport] element:
+//   const ELs_inViewport = document.querySelectorAll('[body]');
+//   ELs_inViewport.forEach(EL => {
+//     Obs.observe(EL, obsOptions);
+//   });
+
 
 
--- jquery.js
+++ jquery.js
@@ -17,9 +17,59 @@ $(function() {
 });
 
 
-// $(function() {
-//     /**
-//     * Smooth scrolling to the top of page !
-//     **/
-//     $("html, body").animate({scrollTop : 0}, 1500);
-// });
+$(function() {
+    /**
+    * Smooth scrolling to the top of page !
+    **/
+    $("html, body").animate({scrollTop : 0}, 1500);
+});
+
+
+const startAnimation = (entries, observer) => {
+    entries.forEach(entry => {
+      entry.target.classList.toggle("slide-in-from-left", entry.isIntersecting);
+    });
+  };
+  
+  const observer = new IntersectionObserver(startAnimation);
+  const options = { root: null, rootMargin: '0px', threshold: 1 }; 
+  
+  const elements = document.querySelectorAll('#boxcontainergauche');
+  elements.forEach(el => {
+    observer.observe(el, options);
+  });
+
+  const startAnimation2 = (entries, observer) => {
+    entries.forEach(entry => {
+      entry.target.classList.toggle("slide-in-from-right", entry.isIntersecting);
+    });
+  };
+  
+  const observer2 = new IntersectionObserver(startAnimation2);
+  const options2 = { root: null, rootMargin: '0px', threshold: 1 }; 
+  
+  const elements2 = document.querySelectorAll('#boxcontainerdroite');
+  elements2.forEach(el => {
+    observer2.observe(el, options2);
+  });
+  
+
+  const startAnimation3 = (entries, observer) => {
+    entries.forEach(entry => {
+      entry.target.classList.toggle("slide-in-from-bot", entry.isIntersecting);
+    });
+  };
+  
+  const observer3 = new IntersectionObserver(startAnimation3);
+  const options3 = { root: null, rootMargin: '0px', threshold: 1 }; 
+  const calcul = 0;
+  
+  const elements3 = document.querySelectorAll('#boxcontainerbot');
+  if (calcul < 20 ){
+    elements3.forEach(el => {
+    observer3.observe(el, options3);
+    // calcul = calcul +1;
+    console.log(calcul);
+  });
+}
+  
