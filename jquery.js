b72faaefc05ff37a74ff6320c08d069abea66531
Florian Bruloy <bruloy.floriangmail.com>
2022-07-20 16:51:41 +0200

    Ajout observer

--- jquery.js
+++ jquery.js
@@ -24,20 +24,32 @@ $(function() {
     $("html, body").animate({scrollTop : 0}, 1500);
 });
 
+var compteurtop = 0;
+// https://w3c.github.io/IntersectionObserver/#dom-intersectionobserver-unobserve 
+
 
 const startAnimation = (entries, observer) => {
+  compteurtop++;
+  console.log(compteurtop);
+
     entries.forEach(entry => {
       entry.target.classList.toggle("slide-in-from-left", entry.isIntersecting);
     });
   };
-  
+
+ 
   const observer = new IntersectionObserver(startAnimation);
   const options = { root: null, rootMargin: '0px', threshold: 1 }; 
   
   const elements = document.querySelectorAll('#boxcontainergauche');
+
+  if ( compteurtop <= 5){ 
   elements.forEach(el => {
     observer.observe(el, options);
-  });
+    // observer.unobserve(document.querySelectorAll('#boxcontainergauche'));
+  })};
+
+
 
   const startAnimation2 = (entries, observer) => {
     entries.forEach(entry => {
@@ -49,11 +61,16 @@ const startAnimation = (entries, observer) => {
   const options2 = { root: null, rootMargin: '0px', threshold: 1 }; 
   
   const elements2 = document.querySelectorAll('#boxcontainerdroite');
+  if ( compteurtop == 5){ 
+    
   elements2.forEach(el => {
-    observer2.observe(el, options2);
+      observer2.observe(el, options2);
+      
   });
-  
+}
 
+
+if ( compteurtop <= 5){
   const startAnimation3 = (entries, observer) => {
     entries.forEach(entry => {
       entry.target.classList.toggle("slide-in-from-bot", entry.isIntersecting);
@@ -65,11 +82,8 @@ const startAnimation = (entries, observer) => {
   const calcul = 0;
   
   const elements3 = document.querySelectorAll('#boxcontainerbot');
-  if (calcul < 20 ){
     elements3.forEach(el => {
     observer3.observe(el, options3);
-    // calcul = calcul +1;
-    console.log(calcul);
   });
 }
   
