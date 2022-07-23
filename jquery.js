3f81631d0103d76718fdfa55389560f2babef012
Florian Bruloy <bruloy.floriangmail.com>
2022-07-23 10:53:00 +0200

    Animation observer resolu

--- jquery.js
+++ jquery.js
@@ -25,57 +25,74 @@ $(function() {
 });
 
 var compteurtop = 0;
-// https://w3c.github.io/IntersectionObserver/#dom-intersectionobserver-unobserve 
+var compteurmid = 0;
+var compteurbot = 0;
 
+// https://w3c.github.io/IntersectionObserver/#dom-intersectionobserver-unobserve 
 
 const startAnimation = (entries, observer) => {
   compteurtop++;
-  console.log(compteurtop);
-
+  if ( compteurtop <= 3){ 
     entries.forEach(entry => {
       entry.target.classList.toggle("slide-in-from-left", entry.isIntersecting);
     });
   };
 
+  if ( compteurtop >= 3){ 
+    entries.forEach(entry => {
+      entry.target.classList.toggle("slide0", entry.isIntersecting);
+    });
+  }};
+
  
   const observer = new IntersectionObserver(startAnimation);
+  const callback = str => { console.log(str); };
   const options = { root: null, rootMargin: '0px', threshold: 1 }; 
   
   const elements = document.querySelectorAll('#boxcontainergauche');
 
-  if ( compteurtop <= 5){ 
+  
   elements.forEach(el => {
     observer.observe(el, options);
-    // observer.unobserve(document.querySelectorAll('#boxcontainergauche'));
-  })};
-
+  });
 
 
   const startAnimation2 = (entries, observer) => {
-    entries.forEach(entry => {
-      entry.target.classList.toggle("slide-in-from-right", entry.isIntersecting);
-    });
-  };
+    compteurmid++;
+    if ( compteurmid <= 3){ 
+      entries.forEach(entry => {
+        entry.target.classList.toggle("slide-in-from-right", entry.isIntersecting);
+      });
+    };
+  
+    if ( compteurmid >= 3){ 
+      entries.forEach(entry => {
+        entry.target.classList.toggle("slide0", entry.isIntersecting);
+      });
+    }};
   
   const observer2 = new IntersectionObserver(startAnimation2);
   const options2 = { root: null, rootMargin: '0px', threshold: 1 }; 
   
   const elements2 = document.querySelectorAll('#boxcontainerdroite');
-  if ( compteurtop == 5){ 
     
   elements2.forEach(el => {
       observer2.observe(el, options2);
-      
   });
-}
 
-
-if ( compteurtop <= 5){
   const startAnimation3 = (entries, observer) => {
-    entries.forEach(entry => {
-      entry.target.classList.toggle("slide-in-from-bot", entry.isIntersecting);
-    });
-  };
+    compteurbot++;
+    if ( compteurbot <= 7){ 
+      entries.forEach(entry => {
+        entry.target.classList.toggle("slide-in-from-bot", entry.isIntersecting);
+      });
+    };
+  
+    if ( compteurbot >= 7){ 
+      entries.forEach(entry => {
+        entry.target.classList.toggle("slide0", entry.isIntersecting);
+      });
+    }};
   
   const observer3 = new IntersectionObserver(startAnimation3);
   const options3 = { root: null, rootMargin: '0px', threshold: 1 }; 
@@ -85,5 +102,4 @@ if ( compteurtop <= 5){
     elements3.forEach(el => {
     observer3.observe(el, options3);
   });
-}
   
